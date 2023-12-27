76d236fa52dd83551a67acda5f5e756181d53557
Stas <stas_sever@mail.ru>
2023-12-27 23:44:18 +0500

    Init test

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 OPERATIONS_PATH = os.path.join(ROOT_PATH, 'src', 'operations.json')
+TEST_FILE_PATH = os.path.join(ROOT_PATH, 'tests', 'test_data.json')
