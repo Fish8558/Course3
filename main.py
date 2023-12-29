290bfed2e1988c660c631ce2e41e93afa7e76c1d
Stas <stas_sever@mail.ru>
2023-12-29 19:45:29 +0500

    Init test

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.utils import json_load, sort_last_five_list, final_information
 
 all_operation_list = json_load(OPERATIONS_PATH)
 last_five_sorted = sort_last_five_list(all_operation_list)
+for i in last_five_sorted:
+    print(i)
 final_information_list = final_information(last_five_sorted)
 
 for operation in final_information_list:
