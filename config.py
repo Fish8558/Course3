5a4c96a6b7c749511bd3a9227fb7626cb26d2638
Stas <stas_sever@mail.ru>
2023-12-30 08:26:01 +0500

    Test new

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 OPERATIONS_PATH = os.path.join(ROOT_PATH, 'src', 'operations.json')
 TEST_FILE_PATH = os.path.join(ROOT_PATH, 'tests', 'test_data.json')
+TEST_FILE_SORTED_PATH = os.path.join(ROOT_PATH, 'tests', 'test_data_sorted.json')
+TEST_FILE_OPERATION_PATH = os.path.join(ROOT_PATH, 'tests', 'test_data_operation.json')
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
-from config import TEST_FILE_PATH
-from main import all_operation_list
-from src import utils
+import json
+
+from config import TEST_FILE_PATH, TEST_FILE_SORTED_PATH, TEST_FILE_OPERATION_PATH
+from src.utils import json_load, sort_last_five_list, final_information
 
 
 def test_json_load():
@@ -9,28 +10,57 @@ def test_json_load():
 
 
 def test_sort_last_five_list():
-    full_list = [
-        {"id": 1, "state": "EXECUTED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 2, "state": "EXECUTED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 3, "state": "CANCELED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 4, "state": "EXECUTED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 5, "state": "CANCELED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 6, "state": "EXECUTED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 7, "state": "EXECUTED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 8, "state": "EXECUTED", "date": "", "from": "", "to": "", "description": "", "operationAmount": ""},
-        {"id": 9, "state": "EXECUTED", "date": "", "": "", "to": "", "description": "", "operationAmount": ""}]
-    last_list = [
-        {'id': 863064926, 'state': 'EXECUTED', 'date': '2019-12-08T22:46:21.935582', 'operationAmount': {'amount': '41096.24', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Открытие вклада', 'to': 'Счет 90424923579946435907'},
-    {'id': 114832369, 'state': 'EXECUTED', 'date': '2019-12-07T06:17:14.634890', 'operationAmount': {'amount': '48150.39', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Visa Classic 2842878893689012', 'to': 'Счет 35158586384610753655'},
-    {'id': 154927927, 'state': 'EXECUTED', 'date': '2019-11-19T09:22:25.899614', 'operationAmount': {'amount': '30153.72', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 7810846596785568', 'to': 'Счет 43241152692663622869'},
-    {'id': 482520625, 'state': 'EXECUTED', 'date': '2019-11-13T17:38:04.800051', 'operationAmount': {'amount': '62814.53', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 38611439522855669794', 'to': 'Счет 46765464282437878125'},
-    {'id': 801684332, 'state': 'EXECUTED', 'date': '2019-11-05T12:04:13.781725', 'operationAmount': {'amount': '21344.35', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Открытие вклада', 'to': 'Счет 77613226829885488381'}
-       ]
-    assert utils.sort_last_five_list(full_list) == last_list
+    with open(TEST_FILE_OPERATION_PATH, encoding='utf-8') as file_json:
+        last_list = json.load(file_json)
+    assert sort_last_five_list(last_list) == [{'date': '2019-12-08T22:46:21.935582',
+                                               'description': 'Открытие вклада',
+                                               'id': 863064926,
+                                               'operationAmount': {'amount': '41096.24',
+                                                                   'currency': {'code': 'USD', 'name': 'USD'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 90424923579946435907'},
+                                              {'date': '2019-12-07T06:17:14.634890',
+                                               'description': 'Перевод организации',
+                                               'from': 'Visa Classic 2842878893689012',
+                                               'id': 114832369,
+                                               'operationAmount': {'amount': '48150.39',
+                                                                   'currency': {'code': 'USD', 'name': 'USD'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 35158586384610753655'},
+                                              {'date': '2019-11-19T09:22:25.899614',
+                                               'description': 'Перевод организации',
+                                               'from': 'Maestro 7810846596785568',
+                                               'id': 154927927,
+                                               'operationAmount': {'amount': '30153.72',
+                                                                   'currency': {'code': 'RUB', 'name': 'руб.'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 43241152692663622869'},
+                                              {'date': '2019-11-13T17:38:04.800051',
+                                               'description': 'Перевод со счета на счет',
+                                               'from': 'Счет 38611439522855669794',
+                                               'id': 482520625,
+                                               'operationAmount': {'amount': '62814.53',
+                                                                   'currency': {'code': 'RUB', 'name': 'руб.'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 46765464282437878125'},
+                                              {'date': '2019-11-05T12:04:13.781725',
+                                               'description': 'Открытие вклада',
+                                               'id': 801684332,
+                                               'operationAmount': {'amount': '21344.35',
+                                                                   'currency': {'code': 'RUB', 'name': 'руб.'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 77613226829885488381'}]
 
 
 def test_final_information():
-    assert utils.final_information('Visa Gold 9447344650495960') == 'Visa Gold 9447 34** **** 5960'
-    assert utils.final_information('Visa Platinum 2241653116508487') == 'Visa Platinum 2241 65** **** 8487'
-    assert utils.final_information('Visa Classic 7022985698476865') == 'Visa Classic 7022 98** **** 6865'
-    assert utils.final_information('Maestro 8045769817179061') == 'Maestro 8045 76** **** 9061'
+    with open(TEST_FILE_SORTED_PATH, encoding='utf-8') as f:
+        last_five_sorted = json.load(f)
+    assert final_information(last_five_sorted) == ['08.12.2019 Открытие вклада\n'
+                                                   'Счет **5907\n41096.24 USD\n',
+                                                   '07.12.2019 Перевод организации\n'
+                                                   'Visa Classic 2842 87** **** 9012 -> Счет **3655\n48150.39 USD\n',
+                                                   '19.11.2019 Перевод организации\n'
+                                                   'Maestro 7810 84** **** 5568 -> Счет **2869\n30153.72 руб.\n',
+                                                   '13.11.2019 Перевод со счета на счет\n'
+                                                   'Счет **9794 -> Счет **8125\n62814.53 руб.\n',
+                                                   '05.11.2019 Открытие вклада\nСчет **8381\n21344.35 руб.\n']
